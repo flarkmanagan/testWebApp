3a735047f24468718f6c064d4900fa8d12b1354b
flarkmanagan <markf121@hotmail.com>
2024-07-10 20:51:12 +0100

    added json example

--- views.py
+++ views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
     return render_template("index.html", name="Mark")
+
+@views.route("/json")
+def get_json():
+    return jsonify({'name': 'mark', 'age': 30})
